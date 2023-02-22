f4a5854231eaa0475e932a4756b008a561df7aeb
Jakub Wieczorek <jakub@grafbase.com>
2023-02-22 17:57:07 +0100

    Work around the message length limit in Cloudflare `trace-events` Logpush job.

--- src/lib.rs
+++ src/lib.rs
@@ -95,31 +95,14 @@ cfg_if! {
         }
 
         fn hook_impl(info: &panic::PanicInfo) {
-            let mut msg = info.to_string();
+            use itertools::Itertools;
+            const CLOUDFLARE_LOGPUSH_MESSAGE_LIMIT: usize = 150 - 7; // Minus the length of the prefix being printed out.
 
-            // Add the error stack to our message.
-            //
-            // This ensures that even if the `console` implementation doesn't
-            // include stacks for `console.error`, the stack is still available
-            // for the user. Additionally, Firefox's console tries to clean up
-            // stack traces, and ruins Rust symbols in the process
-            // (https://bugzilla.mozilla.org/show_bug.cgi?id=1519569) but since
-            // it only touches the logged message's associated stack, and not
-            // the message's contents, by including the stack in the message
-            // contents we make sure it is available to the user.
-            msg.push_str("\n\nStack:\n\n");
-            let e = Error::new();
-            let stack = e.stack();
-            msg.push_str(&stack);
-
-            // Safari's devtools, on the other hand, _do_ mess with logged
-            // messages' contents, so we attempt to break their heuristics for
-            // doing that by appending some whitespace.
-            // https://github.com/rustwasm/console_error_panic_hook/issues/7
-            msg.push_str("\n\n");
-
-            // Finally, log the panic with `console.error`!
-            error(msg);
+            error(format!("PANIC: {info}"));
+            // FIXME: Process the string and split into stack frames.
+            for stack_chunk in Error::new().stack().chars().chunks(CLOUDFLARE_LOGPUSH_MESSAGE_LIMIT).into_iter().map(|chunk| chunk.collect::<String>()) {
+                error(format!("Stack: {stack_chunk}"));
+            }
         }
     } else {
         use std::io::{self, Write};
